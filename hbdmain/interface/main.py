25dba804ff8ac23edd8629112c9e00e3859e8f03
Hugo Guay <dancinoman@gmail.com>
2024-06-03 17:39:19 -0400

    added some files

---
+++ hbdmain/interface/main.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+
+
+def load_data(folder_name):
+    """
+    Put descriptions please of what this function is doing
+    """
+    df = pd.read_csv(folder_name)
+
+    return df
+
+def clean():
+    """
+    Put descriptions please of what this function is doing
+    """
+    
+def preprocess():
+    """
+    Put descriptions please of what this function is doing
+    """
