82b28804b897405e300304accdac84c5da721125
David C. Gale <davidchgale@gmail.com>
2024-06-05 11:39:40 -0400

    update branch

---
+++ package_folder/api_file.py
@@ -0,0 +1,29 @@
+### THIS IS JUST THE TEMPLATE FOR WHEN WE HAVE OUR MODEL
+# dcg
+
+from fastapi import FastAPI
+import pickle
+
+app = FastAPI()
+
+@app.get('/')
+def root():
+    """
+    define the root directory
+    """
+    return {'greeting': 'hello'}
+
+@app.get('/predict')
+def predict(
+			# your predictions for the model etc.
+			)
+    """
+    load the model and store it in the variable 'model'
+    """
+	with open('../models/model_name.pkl', 'wb') as file:
+	model = pickle.load(file)
+
+prediction = model.predict( # features to predict
+							)
+
+return {'prediction': the_prediction}
