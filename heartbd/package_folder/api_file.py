b3e2113251bd935cb15783d3bec9d8e96196a3cc
David C. Gale <davidchgale@gmail.com>
2024-06-10 12:19:42 -0400

    api progress

--- heartbd/package_folder/api_file.py
+++ heartbd/package_folder/api_file.py
@@ -2,14 +2,14 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import pickle
-
+import pandas as pd
 
 # instantiate
 app = FastAPI()
 
 # load model from pickle file
 with open('heartbd/models/main_test.pkl','rb') as file:
-    model = pickle.load(file)
+    app.state.model = pickle.load(file)
 
 # implementing middleware because it is best practice
 app.add_middleware(
@@ -20,40 +20,43 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-_0preRR = '0_pre-RR'
-_0postRR = '0_post-RR'
-_0pPeak = '0_pPeak'
-_0tPeak = '0_tPeak'
-_0rPeak = '0_rPeak'
-_0sPeak = '0_sPeak'
-_0qPeak = '0_qPeak'
-_0qrsinterval = '0_qrs_interval'
-_0pqinterval = '0_pq_interval'
-_0qtinterval = '0_qt_interval'
-_0stinterval = '0_st_interval'
-_0qrsmorph0 = '0_qrs_morph0'
-_0qrsmorph1 = '0_qrs_morph1'
-_0qrsmorph2 = '0_qrs_morph2'
-_0qrsmorph3 = '0_qrs_morph3'
-_0qrsmorph4 = '0_qrs_morph4'
-_1preRR = '1_pre-RR'
-_1postRR = '1_post-RR'
-_1pPeak = '1_pPeak'
-_1tPeak = '1_tPeak'
-_1rPeak = '1_rPeak'
-_1sPeak = '1_sPeak'
-_1qPeak ='1_qPeak'
-_1qrsinterval = '1_qrs_interval'
-_1pqinterval = '1_pq_interval'
-_1qtinterval = '1_qt_interval'
-_1stinterval = '1_st_interval'
-_1qrsmorph0 = '1_qrs_morph0'
-_1qrsmorph1 = '1_qrs_morph1'
-_1qrsmorph2 = '1_qrs_morph2'
-_1qrsmorph3 = '1_qrs_morph3'
-_1qrsmorph4 = '1_qrs_morph4'
-
-class PredictionInput(BaseModel):
+# define a class for the prediction input
+# this is best practice for pydantic data validation (BaseModel is from pydantic)
+class InputFeatures(BaseModel):
+    # define variables relevant to the incoming csv files
+    _0preRR = '0_pre-RR'
+    _0postRR = '0_post-RR'
+    _0pPeak = '0_pPeak'
+    _0tPeak = '0_tPeak'
+    _0rPeak = '0_rPeak'
+    _0sPeak = '0_sPeak'
+    _0qPeak = '0_qPeak'
+    _0qrsinterval = '0_qrs_interval'
+    _0pqinterval = '0_pq_interval'
+    _0qtinterval = '0_qt_interval'
+    _0stinterval = '0_st_interval'
+    _0qrsmorph0 = '0_qrs_morph0'
+    _0qrsmorph1 = '0_qrs_morph1'
+    _0qrsmorph2 = '0_qrs_morph2'
+    _0qrsmorph3 = '0_qrs_morph3'
+    _0qrsmorph4 = '0_qrs_morph4'
+    _1preRR = '1_pre-RR'
+    _1postRR = '1_post-RR'
+    _1pPeak = '1_pPeak'
+    _1tPeak = '1_tPeak'
+    _1rPeak = '1_rPeak'
+    _1sPeak = '1_sPeak'
+    _1qPeak ='1_qPeak'
+    _1qrsinterval = '1_qrs_interval'
+    _1pqinterval = '1_pq_interval'
+    _1qtinterval = '1_qt_interval'
+    _1stinterval = '1_st_interval'
+    _1qrsmorph0 = '1_qrs_morph0'
+    _1qrsmorph1 = '1_qrs_morph1'
+    _1qrsmorph2 = '1_qrs_morph2'
+    _1qrsmorph3 = '1_qrs_morph3'
+    _1qrsmorph4 = '1_qrs_morph4'
+    # now, we assign the data types to the feature variables
     _0preRR: int
     _0postRR: int
     _0pPeak: float
@@ -93,45 +96,89 @@ class PredictionInput(BaseModel):
 def root():
 	return {'greeting': 'ground control to major tom...'}
 
-# define our prediction
-# the 'main_output' variable is just a stand-in for the time being
-# this function loads model from pickled file and stores it in 'model'
-# then sends it to the API
+# define our prediction and call the class of InputFeatures
 @app.get('/predict')
-def predict(
-    try:
-        '0_pre-RR': dtype('int64'),
-        '0_post-RR': dtype('int64'),
-        '0_pPeak': dtype('float64'),
-        '0_tPeak': dtype('float64'),
-        '0_rPeak': dtype('float64'),
-        '0_sPeak': dtype('float64'),
-        '0_qPeak': dtype('float64'),
-        '0_qrs_interval': dtype('int64'),
-        '0_pq_interval': dtype('int64'),
-        '0_qt_interval': dtype('int64'),
-        '0_st_interval': dtype('int64'),
-        '0_qrs_morph0': dtype('float64'),
-        '0_qrs_morph1': dtype('float64'),
-        '0_qrs_morph2': dtype('float64'),
-        '0_qrs_morph3': dtype('float64'),
-        '0_qrs_morph4': dtype('float64'),
-        '1_pre-RR': dtype('int64'),
-        '1_post-RR': dtype('int64'),
-        '1_pPeak': dtype('float64'),
-        '1_tPeak': dtype('float64'),
-        '1_rPeak': dtype('float64'),
-        '1_sPeak': dtype('float64'),
-        '1_qPeak': dtype('float64'),
-        '1_qrs_interval': dtype('int64'),
-        '1_pq_interval': dtype('int64'),
-        '1_qt_interval': dtype('int64'),
-        '1_st_interval': dtype('int64'),
-        '1_qrs_morph0': dtype('float64'),
-        '1_qrs_morph1': dtype('float64'),
-        '1_qrs_morph2': dtype('float64'),
-        '1_qrs_morph3': dtype('float64'),
-        '1_qrs_morph4': dtype('float64')
-            ):
+def predict(X_pred: InputFeatures):
+    '''
+    1. refer to all X features and store in dataframe
+    2. call the model from the pickle
+    3. assuming the model exists in the first place
+    4. convert to np.array if needed
+    5. define our prediction
+    6. return a dictionary formatted as {result: <float>}
+    '''
+    X_pred = pd.DataFrame(locals(), index=[0])
+    model = app.state.model
+    assert model is not None
+    X_processed = X_pred.values
+    y_pred = model.predict(X_processed)
+    return dict(result=float(y_pred))
+
 #     return {'prediction': prediction}        # API-level
 # prediction = model.predict(main_output)  # model-level
+
+# _0preRR = '0_pre-RR'
+# _0postRR = '0_post-RR'
+# _0pPeak = '0_pPeak'
+# _0tPeak = '0_tPeak'
+# _0rPeak = '0_rPeak'
+# _0sPeak = '0_sPeak'
+# _0qPeak = '0_qPeak'
+# _0qrsinterval = '0_qrs_interval'
+# _0pqinterval = '0_pq_interval'
+# _0qtinterval = '0_qt_interval'
+# _0stinterval = '0_st_interval'
+# _0qrsmorph0 = '0_qrs_morph0'
+# _0qrsmorph1 = '0_qrs_morph1'
+# _0qrsmorph2 = '0_qrs_morph2'
+# _0qrsmorph3 = '0_qrs_morph3'
+# _0qrsmorph4 = '0_qrs_morph4'
+# _1preRR = '1_pre-RR'
+# _1postRR = '1_post-RR'
+# _1pPeak = '1_pPeak'
+# _1tPeak = '1_tPeak'
+# _1rPeak = '1_rPeak'
+# _1sPeak = '1_sPeak'
+# _1qPeak ='1_qPeak'
+# _1qrsinterval = '1_qrs_interval'
+# _1pqinterval = '1_pq_interval'
+# _1qtinterval = '1_qt_interval'
+# _1stinterval = '1_st_interval'
+# _1qrsmorph0 = '1_qrs_morph0'
+# _1qrsmorph1 = '1_qrs_morph1'
+# _1qrsmorph2 = '1_qrs_morph2'
+# _1qrsmorph3 = '1_qrs_morph3'
+# _1qrsmorph4 = '1_qrs_morph4'
+
+#  {'0_pre-RR': pd.dtype('int64'),
+#     '0_post-RR': pd.dtype('int64'),
+#     '0_pPeak': pd.dtype('float64'),
+#     '0_tPeak': pd.dtype('float64'),
+#     '0_rPeak': pd.dtype('float64'),
+#     '0_sPeak': pd.dtype('float64'),
+#     '0_qPeak': pd.dtype('float64'),
+#     '0_qrs_interval': pd.dtype('int64'),
+#     '0_pq_interval': pd.dtype('int64'),
+#     '0_qt_interval': pd.dtype('int64'),
+#     '0_st_interval': pd.dtype('int64'),
+#     '0_qrs_morph0': pd.dtype('float64'),
+#     '0_qrs_morph1': pd.dtype('float64'),
+#     '0_qrs_morph2': pd.dtype('float64'),
+#     '0_qrs_morph3': pd.dtype('float64'),
+#     '0_qrs_morph4': pd.dtype('float64'),
+#     '1_pre-RR': pd.dtype('int64'),
+#     '1_post-RR': pd.dtype('int64'),
+#     '1_pPeak': pd.dtype('float64'),
+#     '1_tPeak': pd.dtype('float64'),
+#     '1_rPeak': pd.dtype('float64'),
+#     '1_sPeak': pd.dtype('float64'),
+#     '1_qPeak': pd.dtype('float64'),
+#     '1_qrs_interval': pd.dtype('int64'),
+#     '1_pq_interval': pd.dtype('int64'),
+#     '1_qt_interval': pd.dtype('int64'),
+#     '1_st_interval': pd.dtype('int64'),
+#     '1_qrs_morph0': pd.dtype('float64'),
+#     '1_qrs_morph1': pd.dtype('float64'),
+#     '1_qrs_morph2': pd.dtype('float64'),
+#     '1_qrs_morph3': pd.dtype('float64'),
+#     '1_qrs_morph4': pd.dtype('float64')}
